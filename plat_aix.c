107b4d6eef150474d0fe56ae3016cfc7ebde2660
Dirk Mueller <mueller@kde.org>
2005-10-05 09:03:00 +0000

    SVN_SILENT: fix typo
    svn path=/trunk/KDE/kdemultimedia/kscd/libwm/; revision=467420

--- plat_aix.c
+++ plat_aix.c
@@ -19,7 +19,7 @@
  *
  * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, write to the Free
- * Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  *
  * plat_aix - AIX 4.x IDE and SCSI support  16 Dec 1998
