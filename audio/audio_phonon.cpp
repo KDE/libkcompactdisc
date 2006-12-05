0b65ea188b608fc45798919197a85791fc6eaa1f
Alex Kern <alex.kern@gmx.de>
2006-12-05 21:27:35 +0000

    be consistent in using wm_susleep, define a more large chain, prevent a race in setting current position by CDDA, correct some warnings and debug messages
    Phonon is still not testable!
    
    svn path=/trunk/KDE/kdemultimedia/libkcompactdisc/; revision=610867

--- audio/audio_phonon.cpp
+++ audio/audio_phonon.cpp
@@ -13,6 +13,8 @@
 #include <QThread>
 #include <QByteArray>
 #include <QDataStream>
+#include <QWaitCondition>
+#include <QMutex>
 
 #include <phonon/audiooutput.h>
 #include <phonon/audiopath.h>
@@ -23,74 +25,128 @@ namespace Phonon { class ByteStream; }
 
 class LibWMPcmPlayer : public QThread {
 public:
-    LibWMPcmPlayer() : m_stream(NULL), init_complete(0)
+    LibWMPcmPlayer()
     {
-    };
-
-    void release()
-    {
-        if(m_stream)
-            delete m_stream;
         m_stream = NULL;
-        init_complete = 0;
+        blk = NULL;
     };
 
     ~LibWMPcmPlayer()
     {
-        release();
+        if(m_stream)
+            delete m_stream;
+        m_stream = NULL;
+        blk = NULL;
+
     };
 
     void run()
     {
     };
 
+    void stop()
+    {
+        m_stream->stop();
+    };
+
+    void setNextBuffer(struct cdda_block *new_blk)
+    {
+        mutex.lock();
+
+        blk = new_blk;
+        bufferPlayed.wait(&mutex);
+
+        mutex.unlock();
+    };
+
+public slots:
+    void playNextBuffer()
+    {
+        if(!m_stream) {
+            attachToPhonon();
+        }
+
+        mutex.lock();
+
+        if(blk)
+            m_stream->writeData(QByteArray(blk->buf, blk->buflen));
+        blk = NULL;
+
+        bufferPlayed.wakeAll();
+
+        mutex.unlock();
+    };
+
+private:
+    void attachToPhonon()
+    {
+        Phonon::AudioOutput* m_output = new Phonon::AudioOutput(Phonon::MusicCategory, this);
+        Phonon::AudioPath* m_path = new Phonon::AudioPath(this);
+        m_path->addOutput(m_output);
+        m_stream = new Phonon::ByteStream(this);
+        m_stream->addAudioPath(m_path);
+        m_stream->setStreamSeekable(false);
+        m_stream->setStreamSize(0x7FFFFFFF);
+
+        connect(m_stream, SIGNAL(needData()), this, SLOT(playNextBuffer()));
+
+        QByteArray data;
+        QDataStream stream(&data, QIODevice::WriteOnly);
+        stream.setByteOrder(QDataStream::LittleEndian);
+        stream
+            << 0x46464952 //"RIFF"
+            << static_cast<quint32>(0x7FFFFFFF)
+            << 0x45564157 //"WAVE"
+            << 0x20746D66 //"fmt "           //Subchunk1ID
+            << static_cast<quint32>(16)    //Subchunk1Size
+            << static_cast<quint16>(1)     //AudioFormat
+            << static_cast<quint16>(2)     //NumChannels
+            << static_cast<quint32>(44100 ) //SampleRate
+            << static_cast<quint32>(2*2*44100)//ByteRate
+            << static_cast<quint16>(2*2)   //BlockAlign
+            << static_cast<quint16>(16)    //BitsPerSample
+            << 0x61746164 //"data"              //Subchunk2ID
+            << static_cast<quint32>(0x7FFFFFFF-36)//Subchunk2Size
+            ;
+        m_stream->writeData(data);
+    }
+
     Phonon::ByteStream* m_stream;
-    int init_complete;
+    struct cdda_block *blk;
+    QWaitCondition bufferPlayed;
+    QMutex mutex;
 };
 
-static LibWMPcmPlayer PhononObject;
+static LibWMPcmPlayer *PhononObject = NULL;
 
 int phonon_open(void)
 {
     DEBUGLOG("phonon_open\n");
 
-    Phonon::AudioOutput* m_output = new Phonon::AudioOutput( Phonon::MusicCategory, &PhononObject);
-    Phonon::AudioPath* m_path = new Phonon::AudioPath( &PhononObject );
-    m_path->addOutput( m_output );
-    PhononObject.m_stream = new Phonon::ByteStream( &PhononObject );
-    PhononObject.m_stream->addAudioPath( m_path );
-    PhononObject.m_stream->setStreamSeekable( false );
-    PhononObject.m_stream->setStreamSize( 0x7FFFFFFF );
+    if(PhononObject) {
+        ERRORLOG("Allready initialized!\n");
 
-    QByteArray data;
-    QDataStream stream( &data, QIODevice::WriteOnly );
-    stream.setByteOrder( QDataStream::LittleEndian );
-    stream
-            << 0x46464952 //"RIFF"
-            << static_cast<quint32>( 0x7FFFFFFF )
-            << 0x45564157 //"WAVE"
-            << 0x20746D66 //"fmt "           //Subchunk1ID
-            << static_cast<quint32>( 16 )    //Subchunk1Size
-            << static_cast<quint16>( 1 )     //AudioFormat
-            << static_cast<quint16>( 2 )     //NumChannels
-            << static_cast<quint32>( 44100 ) //SampleRate
-            << static_cast<quint32>( 2*2*44100 )//ByteRate
-            << static_cast<quint16>( 2*2 )   //BlockAlign
-            << static_cast<quint16>( 16 )    //BitsPerSample
-            << 0x61746164 //"data"              //Subchunk2ID
-            << static_cast<quint32>( 0x7FFFFFFF-36 )//Subchunk2Size
-            ;
-    PhononObject.m_stream->writeData(data);
-    PhononObject.init_complete = 1;
+        return -1;
+    }
 
-  return 0;
+    PhononObject = new LibWMPcmPlayer;
+
+    return 0;
 }
 
 int phonon_close(void)
 {
     DEBUGLOG("phonon_close\n");
 
-    PhononObject.release();
+    if(!PhononObject) {
+        ERRORLOG("Unable to close\n");
+        return -1;
+    }
+
+    delete PhononObject;
+
+    PhononObject = NULL;
+
     return 0;
 }
 
@@ -103,15 +159,13 @@ phonon_play(struct cdda_block *blk)
 {
     DEBUGLOG("phonon_play %ld frames, %ld bytes\n", blk->buflen / (2 * 2), blk->buflen);
 
-    if(!PhononObject.init_complete) {
-        if(phonon_open()) {
-            ERRORLOG("Unable to phonon_open()\n");
-            blk->status = WM_CDM_CDDAERROR;
-            return -1;
-        }
+    if(!PhononObject) {
+        ERRORLOG("Unable to play\n");
+        blk->status = WM_CDM_CDDAERROR;
+        return -1;
     }
 
-    PhononObject.m_stream->writeData(QByteArray(blk->buf, blk->buflen));
+    PhononObject->setNextBuffer(blk);
 
     return 0;
 }
@@ -124,7 +178,13 @@ phonon_stop(void)
 {
     DEBUGLOG("phonon_stop\n");
 
-    PhononObject.release();
+    if(!PhononObject) {
+        ERRORLOG("Unable to stop\n");
+        return -1;
+    }
+
+    PhononObject->stop();
+
     return 0;
 }
 
@@ -153,6 +213,8 @@ extern "C" struct audio_oops*
 setup_phonon(const char *dev, const char *ctl)
 {
     DEBUGLOG("setup_phonon\n");
+
+    phonon_open();
 
     return &phonon_oops;
 }
