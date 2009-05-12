c93fd7a077adf1efc8bad488b5a649946ec519b2
Jonathan Riddell <jr@jriddell.org>
2009-05-12 10:09:04 +0000

    revert phonon header change, Qt creates Global now, http://qt.gitorious.org/qt/qt/commit/5299240db14579960358edeebfc72fcef905af13
    svn path=/trunk/KDE/kdemultimedia/libkcompactdisc/; revision=966933

--- phonon_interface.cpp
+++ phonon_interface.cpp
@@ -26,7 +26,7 @@
 #include <kdebug.h>
 #include <klocale.h>
 
-#include <phononnamespace.h>
+#include <Phonon/Global>
 #include <Phonon/MediaObject>
 #include <Phonon/AudioOutput>
 #include <Phonon/Path>
