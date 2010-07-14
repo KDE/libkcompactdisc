KDE/libkcompactdisc

commit b8230031acfe4759bf6a2a9ef37db182a1c8d7ce

deleting things is nice, this might fix the kioslave blocking the cd
svn path=/trunk/KDE/kdemultimedia/libkcompactdisc/; revision=1066399

kcompactdisc.cpp
@@ -179,6 +179,8 @@ KCompactDisc::KCompactDisc(InformationMode infoMode) :
 KCompactDisc::~KCompactDisc()
 {
     stop();
+    delete dummy_ptr;
+    delete d_ptr;
 }
 
 const QString &KCompactDisc::deviceVendor()

commit 3f453b3cb9fee550935bc05f7dd0a98535bad49d

Fix crash introduced by r1066399: dummy_ptr and d_ptr are the same thing.
svn path=/branches/KDE/4.5/kdemultimedia/libkcompactdisc/; revision=1149947

kcompactdisc.cpp
@@ -199,7 +199,6 @@ KCompactDisc::KCompactDisc(InformationMode infoMode) :
 KCompactDisc::~KCompactDisc()
 {
     stop();
-    delete dummy_ptr;
     delete d_ptr;
 }
 