01fd807975f4cc7355a3fd245b5bbb492095c7d8
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-09-14 01:30:53 +0200

    Less auto-casting from char* to QString
    GIT_SILENT

--- tests/testkcd.cpp
+++ tests/testkcd.cpp
@@ -27,7 +27,7 @@ class TestKCD : public QObject
     void doTest()
     {
         qDebug() << "Starting test";
-        mKcd->setDevice(mKcd->defaultCdromDeviceName(), 50, true, "phonon");
+        mKcd->setDevice(mKcd->defaultCdromDeviceName(), 50, true, QStringLiteral("phonon"));
         qDebug() << "";
 
         qDebug() << "We have" << mKcd->audioSystems().size() << "audo systems available:";
